861176407a8005a32f6b24c5792c3970fead9e53
Yatish Kelkar <yatish1606@gmail.com>
2025-08-27 14:43:10 +0530

    Add mapping helper and batch updates

--- src/core/useBeacon.ts
+++ src/core/useBeacon.ts
@@ -1,5 +1,6 @@
 import { useSyncExternalStore, useCallback } from "react";
 import { Store } from "../types";
+import { unstable_batchedUpdates } from "react-dom";
 
 function useBeacon<T, U>(
   store: Store<T>,
@@ -15,7 +16,43 @@ function useBeacon<T, U>(
     getSnapshot
   );
 
-  return [state, () => {}];
+  const stateSetter = (updater: (previousState: U) => U) => {
+    unstable_batchedUpdates(() => {
+      store.set((previousState) => {
+        const selectedState = selector(previousState);
+        const newState =
+          typeof updater === "function" ? updater(selectedState) : updater;
+
+        return {
+          ...previousState,
+          ...mapSelectedStateToGlobalState(selector, newState, previousState),
+        };
+      });
+    });
+  };
+
+  const updateState = useCallback(stateSetter, [store, selector]);
+
+  return [state, updateState];
+}
+
+function mapSelectedStateToGlobalState<T, U>(
+  selector: (state: T) => U,
+  newState: U,
+  prevState: T
+): Partial<T> {
+  const selectedKeys = Object.keys(selector(prevState) as Object);
+  const updates: Partial<T> = {};
+
+  selectedKeys.forEach((key) => {
+    if (key in (newState as Object)) {
+      updates[key as keyof T] = newState[
+        key as keyof U
+      ] as unknown as T[keyof T];
+    }
+  });
+
+  return updates;
 }
 
 export default useBeacon;
